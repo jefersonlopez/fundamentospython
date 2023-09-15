7339149f057f8930f96e2520cdf21b8d32c8c723
jefersonlopez <jeferson_andrey@outlook.com>
2023-09-14 20:29:29 -0500

    primer commit

---
+++ ejemplo.py
@@ -0,0 +1,5 @@
+num1 =input("ingresa el primer numero: ")
+num2 =input("ingresa el segundo numero: ")
+resultado = int(num1) + int(num2)
+print("la suma de losnumeros es: ", resultado)
+#este es el primer comentario
