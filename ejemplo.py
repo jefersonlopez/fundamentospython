fc58a83e810c7a439a989c28221da2e6742cdbc5
jefersonlopez <jeferson_andrey@outlook.com>
2023-09-15 20:56:04 -0500

    se agregan tipos de datos

--- ejemplo.py
+++ ejemplo.py
@@ -1,5 +1,29 @@
+"""
 num1 =input("ingresa el primer numero: ")
 num2 =input("ingresa el segundo numero: ")
-resultado = int(num1) + int(num2)
+num1=int(num1)
+num2=int(num2)
+resultado = num1 + num2
 print("la suma de losnumeros es: ", resultado)
 #este es el primer comentario
+"""
+#string
+my_name = "jeferson"
+print("nombre ", my_name)
+#imprimir el tipo de la variable
+print(type(my_name))
+
+#int
+edad = 27
+print("edad ", edad)
+print(type(edad))
+
+#boolean
+Estado_civil = True
+print("estado ", Estado_civil)
+print(type(Estado_civil))
+
+#inputs
+edad = input("¿Cual es la edad? ")
+print("edad es: ", edad)
+print(type(edad))
