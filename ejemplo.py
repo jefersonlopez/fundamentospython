7fd963e008d2f3bc60fa78feec2302651ad60d66
jefersonlopez <jeferson_andrey@outlook.com>
2023-09-27 20:00:34 -0500

    ciclos anidados

--- ejemplo.py
+++ ejemplo.py
@@ -127,7 +127,7 @@ for key, value in product.items():
 
 print(product.items())
 
-'''
+
 people = [
     {
         'name': 'nico',
@@ -148,3 +148,18 @@ for person in people:
 
 for person in people:
     print(f"name: {person['name']}\nage: {person['age']}")
+    '''
+#ciclos anidados
+
+matriz= [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matriz[0][1])
+
+for row in matriz:
+    print(row)
+    for column in row:
+        print(column)
